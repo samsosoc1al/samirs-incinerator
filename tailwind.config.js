69a742fd0327d8e58738e9f4f6793a07da34f1f7
samsosoc1al <maikelachi0104@gmail.com>
2026-02-07 19:28:07 +0200

    Add Tailwind configuration for SYSOL purple and green theme

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'sysol-purple': '#A78BFA', // Example purple color
+        'sysol-green': '#34D399', // Example green color
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
